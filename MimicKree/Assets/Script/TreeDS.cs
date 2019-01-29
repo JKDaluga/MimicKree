0013c586703aa0f8f92dbd59b9f70c819acce5d6
Jason Daluga <jdaluga@mail.bradley.edu>
2019-01-28 22:05:31 -0600

    init with Tree Data Structure

---
+++ MimicKree/Assets/Script/TreeDS.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class DialogueNode
+{
+
+}
+
+public class TreeDS<T> {
+    private T data;
+    private LinkedList<TreeDS<T>> children;
+
+    public TreeDS(T data)
+    {
+        this.data = data;
+        children = new LinkedList<TreeDS<T>>();
+    }
+
+    public void AddChild(T data)
+    {
+        children.AddFirst(new TreeDS<T>(data));
+    }
+
+    public TreeDS<T> GetChild(int i)
+    {
+        foreach (TreeDS<T> n in children)
+            if (--i == 0)
+                return n;
+        return null;
+    }
+}
