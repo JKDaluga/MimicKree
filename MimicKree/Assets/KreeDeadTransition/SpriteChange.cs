82357d8df572121d7bf6f9266e39bf57b1e1a513
Ninjalex93 <44456463+Ninjalex93@users.noreply.github.com>
2019-02-28 14:28:32 -0600

    Kree CSprite Change and FadeINOUT Animation

---
+++ MimicKree/Assets/KreeDeadTransition/SpriteChange.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteChange : MonoBehaviour
+{
+
+    public Sprite KreeAlive;
+    public Sprite KreeDead;
+
+    float timer = 1f;
+    float delay = 1f;
+
+    void Update()
+    {
+
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+
+            if (this.gameObject.GetComponent<SpriteRenderer>().sprite == KreeAlive)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = KreeDead;
+                timer = delay;
+                return;
+            }
+
+            if (this.gameObject.GetComponent<SpriteRenderer>().sprite == KreeDead)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = KreeAlive;
+                timer = delay;
+                return;
+
+
+            }
+
+        }
+    }
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+
+}
