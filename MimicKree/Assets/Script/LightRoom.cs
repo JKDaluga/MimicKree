8d0143b6151f5dc4766a25455ef221ad882a297d
JKDaluga <31351049+JKDaluga@users.noreply.github.com>
2019-03-12 14:45:03 -0500

    light screen feedback

--- MimicKree/Assets/Script/LightRoom.cs
+++ MimicKree/Assets/Script/LightRoom.cs
@@ -7,6 +7,7 @@ public class LightRoom : Interactable {
 
     public SpriteRenderer[] buttons;
     public SpriteRenderer background;
+    public SpriteRenderer Switch;
 
     public Sprite buttonOn;
     public Sprite buttonOff;
@@ -23,43 +24,40 @@ public class LightRoom : Interactable {
         Player p = FindObjectOfType<Player>();
         while (p.walking)
         {
+            print("1");
             yield return null;
         }
         if (p.location != location)
         {
             yield return null;
         }
-
+        print("2");
         if (p.location == location)
         {
-            print("ACTIVATE");
             toggle = !toggle;
-            if(toggle)
+            toggle = true;
+            if (toggle)
             {
-                foreach(SpriteRenderer button in buttons)
+                foreach (SpriteRenderer button in buttons)
                 {
                     button.sprite = buttonOn;
                 }
                 background.sprite = RoomOn;
+                dialogueEvent.hasDialogue = false;
+                Switch.color = new Color(255f, 255f, 255f);
                 triggerEvent();
             }
-            else
+            /*else
             {
+                print("ACTIVATE");
                 foreach (SpriteRenderer button in buttons)
                 {
                     button.sprite = buttonOff;
                 }
                 background.sprite = RoomOff;
+                Switch.color = new Color(145f, 145f, 145f);
                 triggerEvent(toggleOff);
-            }
-            if (DestoryOnceUsed)
-            {
-                Destroy(this.gameObject);
-            }
-        }
-        else
-        {
-            print("not there ma dude");
+            }*/
         }
     }
 }
