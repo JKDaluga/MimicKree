957279f17f32c6d0024409d6b78ed277c55b8012
JKDaluga <31351049+JKDaluga@users.noreply.github.com>
2019-05-10 00:18:30 -0500

    null reference fix and title screen stuff

--- MimicKree/Assets/Script/SkipButton.cs
+++ MimicKree/Assets/Script/SkipButton.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 public class SkipButton : MonoBehaviour {
     public GameObject skipButton;
-
+    public GameObject skipHelpText;
+    private float counter;
 
 	// Use this for initialization
 	void Start () {
-		
+        counter = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.Escape))
+        if(Input.anyKeyDown)
         {
-            skipButton.SetActive(true);
+            counter = 0;
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                skipButton.SetActive(true);
+            }
+            else
+            {
+                skipHelpText.SetActive(true);
+            }
         }
+        else
+        {
+            counter += Time.deltaTime;
+            if(counter >= 5f)
+            {
+                skipButton.SetActive(false);
+                skipHelpText.SetActive(false);
+            }
+        }
+		
 	}
 }
