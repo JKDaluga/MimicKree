6a91b66e9908605ba45cfc1740abe2bf0fd8a62b
JKDaluga <31351049+JKDaluga@users.noreply.github.com>
2019-05-09 20:41:08 -0500

    big push featuring glowroom

---
+++ MimicKree/Assets/droplet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class droplet : MonoBehaviour {
+
+    public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Splash()
+    {
+        anim.SetBool("Puddle Spash", true);
+    }
+}
